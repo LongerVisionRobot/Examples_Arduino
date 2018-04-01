2c193067ddfea30f738afbe3b5a4da9e8a36b91a
jiapei100 <jp4work@gmail.com>
2018-04-01 14:56:55 -0700

    howtomechatronics/_002_Wifi_ESP8266/_002_Wifi_ESP8266.ino modified

--- howtomechatronics/_002_Wifi_ESP8266/_002_Wifi_ESP8266.ino
+++ howtomechatronics/_002_Wifi_ESP8266/_002_Wifi_ESP8266.ino
@@ -1,24 +1,22 @@
-/*
- ESP8266 Arduino Blink by Simon Peter
- Blink the blue LED on the ESP-01 module
- This example code is in the public domain
- 
- The blue LED on the ESP-01 module is connected to GPIO1 
- (which is also the TXD pin; so we cannot use Serial.print() at the same time)
- 
- Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
-*/
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(0,1);	// RX=0, TX=1
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while(!Serial) {
+    ;	// wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(115200);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because 
-                                    // it is acive low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  if(mySerial.available())
+    Serial.write(mySerial.read());
+  if(Serial.available())
+    mySerial.write(Serial.read());
 }
+
